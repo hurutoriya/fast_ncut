2fe6926bee51669c527a5bc9d4a88631f0b70213
Shunya Ueta <shunyaueta01@gmail.com>
2016-11-19 12:01:54 +0900

    :lipstick: change simple solve case.

--- MATLAB/fast_ncut.m
+++ MATLAB/fast_ncut.m
@@ -16,9 +16,12 @@ function v = fast_ncut(A, constraint_num, MAX_ITERS, OPT_TOL)
 % Refer to:
 %   Xiao-Tong Yuan, Tong Zhang, Truncated Power Method for Sparse Eigenvalue Problems, Technical Report, 2011
 
+fprintf('Fast Ncut ...  \n')
+rng(1,'twister');
+
 %% Init
 if nargin == 0
-    A = diag([1:5]);
+    A = [2 0 0; 0 1 0; 0 0 0];
     MAX_ITERS = 1000;
     OPT_TOL = 10^-8;
     constraint_num = 1;
@@ -30,7 +33,8 @@ dim = size(A,2);
 % You can insert 1 and -1 to i-th and j-th element.
 
 B = zeros(constraint_num, dim-2);
-B = [1 B -1];
+B = [1 -1 B];
+constraint_num = size(B,1);
 c = zeros(constraint_num,1);
 
 %% Fast ncut
@@ -41,7 +45,7 @@ n = B'*(B*B')*c;
 gamma = sqrt(1-norm(n)^2);
 % if c is zero vector, v be a NaN. using any() and escape NaN.
 if any(c) == 0
-    v = P*randn(dim,1);
+    v = randn(dim,1);
 else
     v = gamma * P*A*n/(norm(P*A*n)) + n;
 end
@@ -71,10 +75,11 @@ fprintf('Iteration: %d \n', k)
 fprintf('Residual: %d \n', residual)
 
 %% Result
-figure;
-semilogy(residuals);
-title('Residual')
-xlabel('iteration')
-ylabel('abs(v^{T}Av_{n} - v^{T}Av_{n-1})')
+% figure;
+% semilogy(residuals);
+% title('Residual')
+% xlabel('iteration')
+% ylabel('abs(v^{T}Av_{n} - v^{T}Av_{n-1})')
 
+fprintf('Fast Ncut End \n')
 end
